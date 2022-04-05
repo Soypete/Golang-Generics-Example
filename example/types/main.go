e9d6c580297d5bd29cd7f267cbf4564084c3e1ba
soypete <petersonmiriah@gmail.com>
2022-04-05 11:11:42 -0600

    add struct examples

--- example/types/main.go
+++ example/types/main.go
@@ -5,23 +5,20 @@ import (
 	"unicode/utf8"
 )
 
-type vector[T any] interface {
-	value []T
-	last() ([]T, error)
-}
+type vector[T any] []T
 
-func (v vector[T])last[T any]() ([]T, error) {
-	var zero []T
-	if len(v.value) == 0 {
+func (v vector[T]) last() (T, error) {
+	var zero T
+	if len(v) == 0 {
 		return zero, fmt.Errorf("empty vector")
 	}
 	// TODO: this probably needs to be contraint ordered
-	return v.value[len(v.value)-1], nil
+	return v[len(v)-1], nil
 }
 
 func main() {
 	fmt.Println("vector[int] : ")
-	vGenInt := vector{10, -1}
+	vGenInt := vector[int]{10, -1}
 	i, err := vGenInt.last()
 	if i < 0 {
 		fmt.Println("negative number")
@@ -29,7 +26,7 @@ func main() {
 	fmt.Println("value: %d error: %v\n", i, err)
 
 	fmt.Println("vector[string] : ")
-	vGentStr := vector{"hello", "world"}
+	vGentStr := vector[string]{"hello", "world"}
 	s, err := vGentStr.last()
 	if !utf8.ValidString(s) {
 		fmt.Println("invalid utf8 string")
